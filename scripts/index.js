2ecbb361ed61bc78c742563255324315cad5a842
DESKTOP-K0HS5QI\Dell <vaideska@mail.ru>
2022-06-09 13:29:20 +0300

    refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -6,47 +6,47 @@ const dataStars = [
   {smallRadius: 60, xCenter: 460, yCenter: 460, color: 'black'},
 ];
 
-const canvas = document.querySelector('.figure');
-const ctx = canvas.getContext('2d');
+const largeСanvas = document.querySelector('.large');
+const lgCtx = largeСanvas.getContext('2d');
 
-const canvasLittle = document.querySelector('.little');
-const little = canvasLittle.getContext('2d');
+const smallCanvas = document.querySelector('.small');
+const smCtx = smallCanvas.getContext('2d');
 
 
-const fillLittleCanvas = (color) => {
-  little.fillStyle = color;
-  little.fillRect(0, 0, canvas.width, canvas.height);
+const fillCanvas = (color, canvas, context) => {
+  context.fillStyle = color;
+  context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-const drowStar = ({ smallRadius, xCenter, yCenter, color }) => {
+const drowStar = ({ smallRadius, xCenter, yCenter, color }, context) => {
   const largeRadius = smallRadius * 2;
   const star = new Path2D();
-  ctx.beginPath();
+  context.beginPath();
 
   for (let i = 0; i < 5; i++) {
     star.lineTo(Math.cos( (18+i*72)/180*Math.PI )*largeRadius + xCenter, -Math.sin( (18+i*72)/180*Math.PI )*largeRadius + yCenter);
     star.lineTo(Math.cos( (54+i*72)/180*Math.PI )*smallRadius + xCenter, -Math.sin( (54+i*72)/180*Math.PI )*smallRadius + yCenter);
   }
   star.closePath();
-  ctx.strokeStyle  = color;
-  ctx.fillStyle = color;
-  ctx.fill(star);
-  ctx.stroke(star);
+  context.strokeStyle  = color;
+  context.fillStyle = color;
+  context.fill(star);
+  context.stroke(star);
   return star;
 }
 
 const handleFigureClick = (figure, color) => (event) => {
-  if (ctx.isPointInPath(figure, event.offsetX, event.offsetY)) {
-    fillLittleCanvas(color);
+  if (lgCtx.isPointInPath(figure, event.offsetX, event.offsetY)) {
+    fillCanvas(color, smallCanvas, smCtx);
   } 
 }
 
-canvas.addEventListener('click', () => {
-  fillLittleCanvas('white');
+largeСanvas.addEventListener('click', () => {
+  fillCanvas('white', smallCanvas, smCtx);
 });
 
 dataStars.forEach(data => {
-  const star = drowStar(data);
-  canvas.addEventListener('click', handleFigureClick(star, data.color));
+  const star = drowStar(data, lgCtx);
+  largeСanvas.addEventListener('click', handleFigureClick(star, data.color));
 });
 
